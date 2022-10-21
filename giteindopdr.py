6ce5879bc01629611be2a850eac25558116fc8bc
greedy854 <114736063+greedy854@users.noreply.github.com>
2022-10-21 14:56:58 +0200

    Add files via upload

---
+++ giteindopdr.py
@@ -0,0 +1,45 @@
+print("Storyline") 
+
+print("start") 
+
+print("Part1")
+
+print("A: go mid walk to the store [1]: B: you're gonna go home [2]")
+
+print("Part2")
+
+print("A: getting robbed so grab the gun and start shooting [3] B: go home and look what's wrong [4]")
+
+print("Part3")
+
+print("A: shoot him dead [5] B: go home fast because you see a robber on camera[6]")
+
+print("Part4")
+
+print("A: you're hiding the body in the basement [7] B: you look around home to find the robber[8]")
+
+print("Part5")
+
+print("A: hide the body in the underground closet [9] B: You see the robber and attack him [10]")
+
+print("Part6")
+
+print("A: the body stinks and find some other place [11] B: you kidnapped the robber now at home[12]")
+
+print("Part7")
+
+print("A: you go look around outside [13] B: you put him in a chair in the middle of the room [14]")
+
+print("Part8")
+
+print("A: you find a barn in the garden and put him in there [15] take his mask off [16]")
+
+print("part9")
+
+print("A: put him in the barn and shoot him again [17] B: it is your brother what now [18]")
+
+print("A: then you find out its your brother [19] B: still shoot him dead because you dont care [20]")
+
+print("A: and you're still happy because you hated him [21] B: and you t bag him [22]")
+
+print("END [23]") 
